0444b3af649e09c88176be4b3578eacc1f2e4ff0
izabellagrigoryan <bellagrigoryanmail@gmail.com>
2023-06-24 21:03:15 +0400

    commit of the Interface_Car.cpp

--- Interface_Car.cpp
+++ Interface_Car.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+using namespace std;
 class Car
 {
 public:
@@ -13,8 +14,38 @@ public:
     virtual void parking() = 0;
 };
 
+class BMW :public Car
+{
+private:
+    string horsepower;
+    string capacity;
+    string pressure;
+    string number;
+public:
+    BMW()
+    {
+        this->capacity = "30";
+        this->horsepower = "70";
+        this->pressure = "60";
+        this->number = "AM23456";
+    }
+    void move_front() { cout << "Car move front with horsepower = " << this->horsepower << endl; }
+    void move_left() { cout << "Car move left with horsepower = " << this->horsepower << endl; }
+    void move_right() { cout << "Car move right with horsepower = " << this->horsepower << endl; }
+    void move_back() { cout << "Car move back with pressure = " << this->pressure << endl; }
+    void stop() { cout << "Car stop with capacity = " << this->capacity << endl; }
+    void parking() { cout << "Car parking with number = " << this->number << endl; }
+
+};
+
 int main()
 {
+    BMW my_bmw;
+    my_bmw.move_front();
+    my_bmw.move_left();
+    my_bmw.move_right();
+    my_bmw.move_back();
+    my_bmw.stop();
+    my_bmw.parking();
 
-    std::cout << "Hello World!\n";
 }
